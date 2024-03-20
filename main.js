8324eafe593ca344f12f5b20203e59079853dae2
FurqanEmam <furqanemam@gmail.com>
2024-03-20 07:14:29 +0600

    err handled & enter key press added

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ async function checkWeather(city) {
 
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
   } else {
     var data = await response.json();
     console.log(data);
@@ -45,4 +46,10 @@ async function checkWeather(city) {
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keypress", (event) => {
+  if (event.keyCode == 13) {
+    checkWeather(searchBox.value);
+  }
 });
